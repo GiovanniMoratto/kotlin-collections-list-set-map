df6dd909f6ae936ac7ce33490d824f5d2e499e62
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-14 17:16:24 -0300

    v1.1

--- src/main/kotlin/br/com/alura/main.kt
+++ src/main/kotlin/br/com/alura/main.kt
@@ -5,7 +5,28 @@ package br.com.alura
  */
 
 fun main() {
-    val nomes: List<String> = listOf(
+    val banco = BancoDeNomes()
+//    banco.nomes.add("Alex")
+    banco.salva("Alex")
+    println(banco.nomes)
+    println(BancoDeNomes().nomes)
+}
+
+class BancoDeNomes {
+
+    val nomes: Collection<String> get() = dados
+
+    fun salva(nome: String) {
+        dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val nomes: Collection<String> = mutableListOf(
         "Alex",
         "Fran",
         "Gui",
